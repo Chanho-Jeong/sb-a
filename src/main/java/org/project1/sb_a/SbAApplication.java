104f5d3fe9299e0f75331f49ef71fe03ab8cf869
Pro <yjyk0123@naver.com>
2025-04-17 13:30:16 +0900

    first commit

---
+++ src/main/java/org/project1/sb_a/SbAApplication.java
@@ -0,0 +1,13 @@
+package org.project1.sb_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project1/sb_a/SbAApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project1.sb_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
